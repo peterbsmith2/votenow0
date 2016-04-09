9dc83c0263ea251e460ab0af37d3de695ffc468e
Peter B Smith <peterbutlersmith@gmail.com>
2016-04-09 13:20:30 -0400

    stubbing slack route

--- server.js
+++ server.js
@@ -9,6 +9,23 @@ app.use(morgan("dev"));
 // enable POST request body parsing
 app.use(bodyParser.json());
 
+
+app.post('/slack', function(req,res) {
+  
+  var text = req.body.text;
+  var response;
+
+  
+
+
+
+
+
+  res.json(response);
+
+})
+
+
 app.listen(process.env.PORT || 3000, () => {
   console.log("Listening on", process.env.PORT || 3000);
 });
