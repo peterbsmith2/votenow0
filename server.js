a74503a0d31a6dec47e5959fa364b8c73eac648d
1egoman <rsg1egoman@gmail.com>
2016-04-09 13:07:15 -0400

    listen on port

--- server.js
+++ server.js
@@ -3,13 +3,12 @@ var app = express();
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 
-
 // enable developer logs
 app.use(morgan("dev"));
 
 // enable POST request body parsing
 app.use(bodyParser.json());
 
-
-
-app.listen
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Listening on", process.env.PORT || 3000);
+});
